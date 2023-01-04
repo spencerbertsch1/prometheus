7be19d25302e0de57975438813c9df71b0adb9ec
Spencer Bertsch <spencerbertsch99@gmail.com>
2023-01-04 10:57:29 -0500

    add TODOs

---
+++ settings.py
@@ -0,0 +1,20 @@
+"""
+-------------------------------
+| Dartmouth College           |
+| Prometheus                  |
+| Winter 2022                 |
+| Spencer Bertsch             |
+-------------------------------
+
+This settings file contains basic utilities and information required by python scripts through out
+this project. 
+
+Tools such as the logger and information such as the dynamic path to the wildfire datasets 
+are stored here and easily accessed from anywhere in the project. 
+"""
+
+
+# TODO create logger 
+
+# TODO define dynamic paths to data directory 
+
