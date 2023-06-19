e8e53d1a509eb47127e5705ac503c76a83ee32f7
Spencer Bertsch <spencerbertsch99@gmail.com>
2023-06-18 20:24:36 -0400

    update animation script

--- src/fire_sim_iter.py
+++ src/fire_sim_iter.py
@@ -14,54 +14,11 @@ https://thecleverprogrammer.com/2020/07/26/openai-gym-in-machine-learning/
 
 from matplotlib import pyplot as plt
 import numpy as np
-from matplotlib.animation import FuncAnimation 
 from matplotlib import animation
    
-# # initializing a figure in 
-# # which the graph will be plotted
-# fig = plt.figure() 
-   
-# # marking the x-axis and y-axis
-# axis = plt.axes(xlim =(0, 4), 
-#                 ylim =(-2, 2)) 
-  
-# # initializing a line variable
-# line, = axis.plot([], [], lw = 3) 
-   
-# # data which the line will 
-# # contain (x, y)
-# def init(): 
-#     line.set_data([], [])
-#     return line,
-   
-# def animate(i):
-#     x = np.linspace(0, 4, 1000)
-   
-#     # plots a sine graph
-#     y = np.sin(2 * np.pi * (x - 0.01 * i))
-#     line.set_data(x, y)
-      
-#     return line,
-   
-# anim = FuncAnimation(fig, animate, init_func = init,
-#                      frames = 200, interval = 20)
-  
-# plt.show()
-
-
-
-# anim.save('continuousSineWave.mp4', 
-#           writer = 'ffmpeg', fps = 30)
-
-fig = plt.figure() 
-   
-# marking the x-axis and y-axis
-axis = plt.axes(xlim =(0, 4), 
-                ylim =(-2, 2)) 
-
 frames = []
-line, = axis.plot([], [], lw = 3) 
 
+# run the RL algorithm and get the frames of the environment state 
 done = False
 for step in range(200):
     rand_arr = np.random.rand(3, 3)
@@ -72,20 +29,33 @@ for step in range(200):
     if done:
         break
 
+# define helper function
 def update_scene(num, frames, patch):
     patch.set_data(frames[num])
     return patch,
 
-def plot_animation(frames, repeat=False, interval=40):
+def plot_animation(frames, repeat: bool, interval: int, save_anim: bool, show_anim: bool):
+    """
+    Function to plot the series of environment frames generated during training 
+    """
     fig = plt.figure()
     patch = plt.imshow(frames[0])
     plt.axis('off')
     anim = animation.FuncAnimation(
         fig, update_scene, fargs=(frames, patch),
         frames=len(frames), repeat=repeat, interval=interval)
-    plt.show()
-    # plt.close()
+    
+    if save_anim: 
+        fps = int(round(1000/interval))
+        anim.save('test_anim.mp4', writer='ffmpeg', fps=fps)
+
+    if show_anim: 
+        plt.show()
 
     return anim
 
-plot_animation(frames)
+repeat = False  # repeat the animation
+interval = 100  # millisecond interval for each frame in the animation
+save_anim = True  # save the animation to disk 
+show_anim = True
+plot_animation(frames, repeat=repeat, interval=interval, save_anim=True, show_anim=show_anim)
