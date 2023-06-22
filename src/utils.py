b44a6d38467972c84b3996871284f9e4a7c67e9a
Spencer Bertsch <spencerbertsch99@gmail.com>
2023-06-22 18:24:28 -0400

    update colors in the animations

--- src/utils.py
+++ src/utils.py
@@ -45,8 +45,20 @@ def plot_animation(frames, repeat: bool, interval: int, save_anim: bool, show_an
     """
     Function to plot the series of environment frames generated during training 
     """
+    # Colours for visualization: brown for EMPTY, dark green for TREE and orange
+    # for FIRE. Note that for the colormap to work, this list and the bounds list
+    # must be one larger than the number of different values in the array.
+    # EMPTY, TREE, FIRE, AIRCRAFT, PHOSCHEK, AIRPORT = 0, 1, 2, 3, 4, 5
+    if EnvParams.fire_speed != 1:
+         frames = frames[0::EnvParams.fire_speed]
+    
+    colors_list = ['black', 'forestgreen', 'orange', 'white', 'red', 'lightblue']
+    cmap = colors.ListedColormap(colors_list)
+    bounds = [0, 1, 2, 3, 4, 5]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+
     fig = plt.figure()
-    patch = plt.imshow(frames[0])
+    patch = plt.imshow(frames[0], cmap=cmap, interpolation='nearest')
     plt.axis('off')
     anim = animation.FuncAnimation(
         fig, update_scene, fargs=(frames, patch),
@@ -81,3 +93,38 @@ def viewer(X: np.array):
         display.blit(surf, (0, 0))
         pygame.display.update()
     pygame.quit()
+
+
+class Aircraft():
+    aircraft_type = 'Aircraft'
+    def __init__(self, fuel_level: float = 1.0, phoschek_level: float = 1.0, curr_direction: str = 'N', 
+                     dropping_phoschek: bool = False, location: list = [1, 1], curr_terrain = 0):
+        self.fuel_level = fuel_level
+        self.phoschek_level = phoschek_level
+        self.curr_direction = curr_direction
+        self.dropping_phoschek = dropping_phoschek
+        self.location = location
+        self.curr_terrain = curr_terrain
+
+    def __repr__(self):
+        print_str = f'''
+                      {self.aircraft_type} fuel level: {round(self.fuel_level * 100, 2)}% 
+                      {self.aircraft_type} phoschek level: {round(self.phoschek_level * 100, 2)}%
+                      {self.aircraft_type} current direction: {self.curr_direction}
+                      {self.aircraft_type} currently dropping phoschek: {self.dropping_phoschek}
+                      {self.aircraft_type} location: {self.location}
+                      '''
+        print(print_str)
+        
+class Helicopter(Aircraft):
+        aircraft_type = 'Helicopter'
+        def __init__(self, fuel_level: float = 1.0, phoschek_level: float = 1.0, curr_direction: str = 'N', 
+                     dropping_phoschek: bool = False, location = [1, 1]):
+            super().__init__(fuel_level, phoschek_level, curr_direction, dropping_phoschek, location)
+
+
+class Plane(Aircraft):
+        aircraft_type = 'Plane'
+        def __init__(self, fuel_level: float = 1.0, phoschek_level: float = 1.0, curr_direction: str = 'N', 
+                     dropping_phoschek: bool = False, location = [1, 1]):
+            super().__init__(fuel_level, phoschek_level, curr_direction, dropping_phoschek, location)
